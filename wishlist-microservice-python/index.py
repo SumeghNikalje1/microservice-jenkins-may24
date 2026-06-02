b40da39828e319009c9754e90c94ed46364e27c1
SumeghNikalje1 <sumegh.nikalje@outlook.com>
2026-06-02 12:25:18 +0530

    Update index.py

--- wishlist-microservice-python/index.py
+++ wishlist-microservice-python/index.py
@@ -1,38 +1,30 @@
-from flask import Flask,request
-from flask_cors import CORS 
-import json
-
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-
-@app.route('/', methods = ['GET'])
+@app.route('/', methods=['GET'])
 def hello():
-   print('Getting List of WishList Items')
-   x = {
-    "1": "Apple Iphone",
-    "2": "MacBook",
-    "3": "Your Fav Something else"
-   }
-   y = json.dumps(x)
-   return y
+    print('Getting List of WishList Items')
+    x = {
+        "1": "Apple Iphone",
+        "2": "MacBook",
+        "3": "Your Fav Something else"
+    }
+    return jsonify(x)
 
-
-@app.route('/likes', methods = ['GET'])
+@app.route('/likes', methods=['GET'])
 def likes():
-   print('Getting List of WishList Items')
-   return 'List of WishList Items'
+    return 'List of WishList Items'
 
-
-@app.route('/product/<product>', methods = ['GET', 'POST'])
+@app.route('/product/<product>', methods=['GET', 'POST'])
 def product(product):
-   if request.method == 'POST':
-      print(product)
-      return product
-   return 'Call from POST'
-
+    if request.method == 'POST':
+        print(product)
+        return product
+    return 'Call from POST'
 
 if __name__ == '__main__':
-    app.run(port='1003')
-    print('Wishlist Microservice Started...')
+    print("Wishlist Microservice Started...")
+    app.run(host="0.0.0.0", port=1003)
